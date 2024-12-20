93d51519296137ca998ee868201bc31ab824faf0
Talha Javaid Mlik <malik@Talhas-MacBook-Air.local>
2024-12-20 20:11:58 +0500

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lovely-flamingo-139.convex.cloud'
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.shutterstock.com'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
