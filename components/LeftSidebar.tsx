335d15d8bc37199720cfda462119280c534ccc63
Talha089 <talhajavaidmalik@gmail.com>
2024-12-30 18:39:38 +0500

    RighSidebar

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -8,11 +8,14 @@ import { cn } from '@/lib/utils'
 import React from 'react'
 import { sidebarLinks } from '@/constants'
 import { usePathname, useRouter } from 'next/navigation'
+import { SignedIn, SignedOut, useClerk } from '@clerk/nextjs'
+import { Button } from './ui/button'
 
 const LeftSidebar = () => {
     const pathname = usePathname();
     const router = useRouter();
 
+    const { signOut } = useClerk();
     return (
         <section className='left_sidebar'>
             <nav className='flex flex-col gap-6'>
@@ -25,14 +28,31 @@ const LeftSidebar = () => {
                     const isActive = pathname === route || pathname.startsWith
                         (`${route}/`);
 
-                    return  <Link href={route} key={label} className={cn("flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start", {
+                    return <Link href={route} key={label} className={cn("flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start", {
                         'bg-nav-focus border-r-4 border-orange-1': isActive
-                      })}>
+                    })}>
                         <Image src={imgURL} alt={label} width={24} height={24} />
                         <p>{label}</p>
-                      </Link>
+                    </Link>
                 })}
             </nav>
+            <SignedOut>
+                <div className='flex-center w-full pb-14 max-lg:px-4 lg:-8'>
+                    <Button asChild className='text-16 w-full bg-orange-1 font-extrabold'>
+                        <Link href='/sign-in'>Sign in</Link>
+                    </Button>
+                </div>
+            </SignedOut>
+
+
+            <SignedIn>
+                <div className='flex-center w-full pb-14 max-lg:px-4 lg:-8'>
+                    <Button className='text-16 w-full bg-orange-1 font-extrabold'
+                        onClick={() => signOut(() => router.push('/'))}>
+                        Log Out
+                    </Button>
+                </div>
+            </SignedIn>
         </section>
     )
 }
